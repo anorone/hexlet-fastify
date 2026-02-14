3daddcb59f3874d88ffac56f22d561b094b5e35e
zhenia-chugaev <zhenia.chugaev@bk.ru>
2026-02-14 15:15:16 +0100

    Add request handlers

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ app.get('/', (request, reply) => {
   reply.send('Hello, World!');
 });
 
+app.get('/users', (request, reply) => {
+  reply.send('GET /users');
+});
+
+app.post('/users', (request, reply) => {
+  reply.send('POST /users');
+});
+
 app.listen({ port }, () => {
   console.log(`Example app listening on port ${port}`)
 });
