34d7ec118fb0257907297a93f2a324311282d180
zhenia-chugaev <zhenia.chugaev@bk.ru>
2026-04-04 12:51:05 +0200

    Add form data validation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import fastify from 'fastify';
 import viewPlugin from '@fastify/view';
 import formBodyPlugin from '@fastify/formbody';
 import pug from 'pug';
+import * as Yup from 'yup';
 import { state } from './state.js';
 
 const port = 3000;
@@ -38,7 +39,8 @@ app.get('/courses', (request, reply) => {
 });
 
 app.get('/courses/new', (request, reply) => {
-  reply.view('courses/new');
+  const locals = { values: {}, error: null };
+  reply.view('courses/new', locals);
 });
 
 app.get('/courses/:courseId', (request, reply) => {
@@ -51,11 +53,37 @@ app.get('/courses/:courseId/lessons/:lessonId', (request, reply) => {
   reply.send(`Course ID: ${courseId}; Lesson ID: ${lessonId}`);
 });
 
-app.post('/courses', (request, reply) => {
+app.post('/courses', {
+  attachValidation: true,
+  schema: {
+    body: Yup.object().shape({
+      title: Yup.string().trim().min(2),
+      description: Yup.string().trim().min(10),
+    }),
+  },
+  validatorCompiler: ({ schema }) => (request) => {
+    try {
+      const value = schema.validateSync(request);
+      return { value };
+    } catch (error) {
+      return { error };
+    }
+  },
+}, (request, reply) => {
+  const { validationError } = request;
+  if (validationError) {
+    const locals = {
+      values: request.body,
+      error: validationError,
+    };
+    reply.view('courses/new', locals);
+    return;
+  }
+  const { title, description } = request.body;
   const course = {
     id: state.courses.length + 1,
-    title: request.body.title.trim(),
-    description: request.body.description.trim(),
+    title,
+    description,
   };
   state.courses.push(course);
   reply.redirect('/courses');
@@ -67,7 +95,8 @@ app.get('/users', (request, reply) => {
 });
 
 app.get('/users/new', (request, reply) => {
-  reply.view('/users/new');
+  const locals = { values: {}, error: null };
+  reply.view('/users/new', locals);
 });
 
 app.get('/users/:userId', (request, reply) => {
@@ -86,12 +115,44 @@ app.get('/users/:userId/posts/:postId', (request, reply) => {
   reply.send(`User ID: ${userId}; Post ID: ${postId}`);
 });
 
-app.post('/users', (request, reply) => {
+app.post('/users', {
+  attachValidation: true,
+  schema: {
+    body: Yup.object().shape({
+      name: Yup.string().trim().min(2, 'Name must be at least 2 characters'),
+      email: Yup.string().email().trim().lowercase(),
+      password: Yup.string().min(5),
+      passwordConfirmation: Yup.string().test(
+        'passwords are equal',
+        'Password and password confirmation must be equal',
+        (value, { parent }) => value === parent.password,
+      ),
+    }),
+  },
+  validatorCompiler: ({ schema }) => (request) => {
+    try {
+      const value = schema.validateSync(request);
+      return { value };
+    } catch (error) {
+      return { error };
+    }
+  },
+}, (request, reply) => {
+  const { validationError } = request;
+  if (validationError) {
+    const locals = {
+      values: request.body,
+      error: validationError,
+    };
+    reply.view('/users/new', locals);
+    return;
+  }
+  const { name, email, password } = request.body;
   const user = {
     id: state.users.length + 1,
-    name: request.body.name.trim(),
-    email: request.body.email.trim().toLowerCase(),
-    password: request.body.password,
+    name,
+    email,
+    password,
   };
   state.users.push(user);
   reply.redirect('/users');
